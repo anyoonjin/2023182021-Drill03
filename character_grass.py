75cf669e57afc5b2aefcdf0a4eaac4f03ae8574b
yunjinan@MYPC <anyoonjin0@gmail.com>
2024-09-24 11:49:52 +0900

    뼈대 완성, 프린트로 무한루프 구조 확인

--- character_grass.py
+++ character_grass.py
@@ -5,12 +5,23 @@ open_canvas()
 grass=load_image('grass.png')
 character=load_image('character.png')
 
-x=0
-while(x<800):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x,90)
-    x=x+2
-    delay(0.01)
+def run_circle():
+    print('circle')
+    pass
+
+
+
+    
+def run_rectangle():
+    print('rectangle')
+    pass
+
+
+
+    
+
+while True:
+    run_circle()
+    run_rectangle()
 
 close_canvas()
